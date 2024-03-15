fc8bd377c119ed46dbf4758e56f8fc9cc1baadd8
Phạm Văn Phúc <157782146+PhucHandSom3@users.noreply.github.com>
2024-03-15 09:17:42 +0700

    LapTrinhMang_PhamVanPhuc_1571020203

---
+++ Tuan9/RMIClient.java
@@ -0,0 +1,21 @@
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class RMIClient {
+    public static void main(String[] args) {
+        try {
+            // Tìm kiếm registry trên cổng 1099
+            Registry registry = LocateRegistry.getRegistry("localhost", 12345);
+
+            // Lấy đối tượng calculator từ registry
+            Calculator calculator = (Calculator) registry.lookup("CalculatorService");
+
+            // Gọi phương thức từ xa
+            int result = calculator.add(8, 9);
+            System.out.println("Ket qua: " + result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
